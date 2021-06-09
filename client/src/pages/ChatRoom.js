c5bc3f196009f8691a081ce3db6a786a2f62133b
Joey Lansangan <joeylansangan@gmail.com>
2021-06-09 18:03:47 -0500

    initial style setup

--- client/src/pages/ChatRoom.js
+++ client/src/pages/ChatRoom.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/ChatRoom.css';
 
 function ChatRoom(){
     return(
--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './styles/SignIn.css';
 
 function SignIn(){
     return(
         <div className="wrapper">
-            <img src="./assets/images/logoname.svg" alt="jabberbox"/>
+            <img id="logo-name"src="./assets/images/logoname.svg" alt="jabberbox"/>
+
+            <div>
+                <h4>Welcome!</h4>
+                <h3>Come in and join a room</h3>
+            </div>
         </div>
     )
 }
